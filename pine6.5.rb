c2029d40756226317470726e6aa8dbe4f45a5c5f
John McCormack <jmccormack321@gmail.com>
2012-05-15 21:31:23 +0100

    commit of exercise frm code dir dropbox

---
+++ pine6.5.rb
@@ -0,0 +1,27 @@
+puts rand
+puts rand
+puts rand
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(1)
+puts rand(1)
+puts rand(1)
+puts rand(54547815154512345451215055645565654474585156454259)
+puts "there is a " + rand(101).to_s + "% chnace of that happening."
+puts ''
+puts "There is one chance in " + rand(5454789787845158451514245456555).to_s + " of that happening."
+
+srand 50
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts''
+srand 50
+puts rand(100)
+puts rand(100)
+puts rand(100)
+srand
+puts rand(100)
+puts rand(100)
+puts rand(100)
